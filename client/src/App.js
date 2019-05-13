91a2bde9bff9682254cf41471491bf01db828118
Hussein Azeez <hussein.azeez91@gmail.com>
2019-05-13 20:35:44 +0700

    run eslint on these files

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 // Third-party imports
-import React from 'react'
+import React from 'react';
 
 // Local imports
-import DataProvider from './context/Data'
-import TerminateModalFlow from './components/TerminateModalFlow'
+import { DataProvider } from './context/Data';
+import TerminateModalFlow from './components/TerminateModal';
 
 const App = () => {
   return (
     <DataProvider>
       <TerminateModalFlow />
     </DataProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
