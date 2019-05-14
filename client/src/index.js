88cc59e527cc67f68777246d33f03795a6039b7a
Hussein Azeez <hussein.azeez91@gmail.com>
2019-05-14 22:42:18 +0700

    Remove the mokeDataProvider import

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 // Third-party imports
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Local imports
-import './index.css'
-import App from './App'
+import './index.css';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // Hot Module Replacement
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
